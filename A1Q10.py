273762785413d68749a3b495ee1a9b825cac7403
Sudhirgupta123 <111416823+Sudhirgupta123@users.noreply.github.com>
2022-08-18 23:56:55 +0530

    Add files via upload

---
+++ A1Q10.py
@@ -0,0 +1,8 @@
+'''10. Write a python script to print values of three variables, each in a new line. Variable
+contains integer values'''
+x=3
+y=2
+z=4.5
+print(x)
+print(y)
+print(z)
---
+++ A1Q9.py
@@ -0,0 +1,4 @@
+'''Write a python script to create variables to store your name, age, qualification, years
+of experience and print it.'''
+x="sudhir gupta","age=22","qualification=btech","year of experience=2"
+print(x)
