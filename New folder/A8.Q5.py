43525ef09352ede5fcb6b83781422dc921fef20d
Sudhir gupta <111416823+Sudhirgupta123@users.noreply.github.com>
2023-01-20 19:26:05 +0530

    Add files via upload

---
+++ New folder/A8.Q5.py
@@ -0,0 +1,5 @@
+#5. Write a python script to print first 10 odd natural numbers in reverse order
+i=1
+while (i<20):
+    print(20-i)
+    i=i+2
---
+++ New folder/A8.Q7.py
@@ -0,0 +1,5 @@
+#7. Write a python script to print first 10 even natural numbers in reverse order
+i=2
+while (i<=20):
+    print(20-i)
+    i=2+i
